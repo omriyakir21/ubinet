45e5bc911a832e6160e20f5ac6c514d68411a1ad
omri yakir <omriyakir@mail.tau.ac.il>
2024-08-03 13:18:58 +0300

    adding sys path

--- data_preparation/ScanNet/db_creation_scanNet.py
+++ data_preparation/ScanNet/db_creation_scanNet.py
@@ -2,7 +2,7 @@ import copy
 import os
 import sys
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..','..'))
 import pickle
 
 import numpy as np
