589fbfa74acb06f071153e3da299a2d56f365b92
omri yakir <omriyakir@mail.tau.ac.il>
2024-08-05 00:44:52 +0300

    fix 3by4 path

--- data_preparation/ScanNet/db_creation_scanNet.py
+++ data_preparation/ScanNet/db_creation_scanNet.py
@@ -2,7 +2,7 @@ import copy
 import os
 import sys
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '..','..'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
 import pickle
 
 import numpy as np
@@ -31,6 +31,7 @@ def download_assemblies_and_assymetrics(PDB_names_list):
     db_utils.download_assembly_files(PDB_names_list, pdb_list_object, paths.assemblies_path)
     return
 
+
 def create_save_list_of_entry_dicts():
     '''
     :param listOfPDBNames: list of PDB names
@@ -50,7 +51,6 @@ def create_save_list_of_entry_dicts():
     db_utils.save_as_pickle(list_of_entry_dicts, os.path.join(paths.entry_dicts_path, 'list_of_entry_dicts.pkl'))
 
 
-
 if __name__ == "__main__":
     # retrive the list of PDB names
     # PDB_names_list = db_utils.read_PDB_names_from_file(UBIQ_LIST_PATH)
@@ -58,23 +58,24 @@ if __name__ == "__main__":
     # download_assemblies_and_assymetrics(PDB_names_list)
     # create_save_list_of_entry_dicts()
 
-
-    ubiq_path = '3by4.cif'
+    ubiq_path = os.path.join(paths.pdbs_path, '3by4.cif')
     ubiq_structure = db_utils.parser.get_structure('3BY4', ubiq_path)
     ubiq_chain = ubiq_structure[0]['B']  # uni-prot = "UBIQ-HUMAN"
     ubiq_seq = db_utils.get_str_seq_of_chain(ubiq_chain)
     ubiq_amino_acids = db_utils.aa_out_of_chain(ubiq_chain)
     ubiq_atoms = db_utils.get_atoms_of_amino_acids(ubiq_chain)
     ubiqDiameter = db_utils.calculate_diameter(ubiq_atoms)
-    ubiq_residues_list = [db_utils.THREE_LETTERS_TO_SINGLE_AA_DICT[str(aminoAcid.get_resname())] + str(aminoAcid.get_id()[1]) for aminoAcid
-                          in ubiq_amino_acids]
+    ubiq_residues_list = [
+        db_utils.THREE_LETTERS_TO_SINGLE_AA_DICT[str(aminoAcid.get_resname())] + str(aminoAcid.get_id()[1]) for
+        aminoAcid
+        in ubiq_amino_acids]
 
     # list_of_entry_dicts = db_utils.load_as_pickle(os.path.join(paths.entry_dicts_path, 'list_of_entry_dicts.pkl'))
     # db_utils.pickle_list_of_entry_dicts(list_of_entry_dicts, 852)
     # Queen_predictions_path = db_utils.load_as_pickle(os.path.join(paths.QUEEN_path, 'predictions'))
 
-    chosen_assemblies = db_utils.from_pickle_to_choose_assemblies(os.path.join(paths.entry_dicts_path, 'entry_dicts_with_probabilities.pkl'))
+    chosen_assemblies = db_utils.from_pickle_to_choose_assemblies(
+        os.path.join(paths.entry_dicts_path, 'entry_dicts_with_probabilities.pkl'))
     # chosenAssembliesListOfSublists = db_utils.split_list(chosen_assemblies, 40)
     # items = [(chosenAssembliesListOfSublists[i], i) for i in range(40)]
     # createDataBase(items[14])
-
