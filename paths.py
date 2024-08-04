e75b473ff6b2ff9b86a1def01f9bf322344b7786
omri yakir <omriyakir@mail.tau.ac.il>
2024-08-04 22:41:07 +0300

    key and pqths update

--- paths.py
+++ paths.py
@@ -5,25 +5,55 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 
 # data preparation paths
 data_preparation_path = os.path.join(current_dir, 'data_preparation')
+#   patch_to_score
+patch_to_score_path = os.path.join(data_preparation_path, 'patch_to_score')
+mmseqs_patch_to_score_path = os.path.join(patch_to_score_path, 'mmseqs')
+
+#   ScanNet
 ScanNet_data_preparation_path = os.path.join(data_preparation_path, 'ScanNet')
+mmseqs_scanNet_path = os.path.join(ScanNet_data_preparation_path, 'mmseqs')
+mafft_path = os.path.join(ScanNet_data_preparation_path, 'mafft')
+ASA_path = os.path.join(ScanNet_data_preparation_path, 'ASA')
+ImerFiles_path = os.path.join(ScanNet_data_preparation_path, 'ImerFiles')
+
+
 # datasets paths
 datasets_path = os.path.join(current_dir, 'datasets')
+#   patch_to_score
+patch_to_score_dataset_path = os.path.join(datasets_path, 'patch_to_score')
+#       sources
+patch_to_score_sources_path = os.path.join(patch_to_score_dataset_path, 'sources')
+#           GO
+GO_source_patch_to_score_path = os.path.join(patch_to_score_sources_path, 'GO')
+#               ubiquitin_binding
+ubiquitin_binding_path = os.path.join(GO_source_patch_to_score_path, 'ubiquitin_binding')
+#               E1
+E1_path = os.path.join(GO_source_patch_to_score_path, 'E1')
+#               E2
+E2_path = os.path.join(GO_source_patch_to_score_path, 'E2')
+#               E3
+E3_path = os.path.join(GO_source_patch_to_score_path, 'E3')
+#               DUB
+DUB_path = os.path.join(GO_source_patch_to_score_path, 'DUB')
+#           AFDB
+AFDB_source_patch_to_score_path = os.path.join(patch_to_score_sources_path, 'AFDB')
+#   scanNet
 datasets_scanNet_path = os.path.join(datasets_path, 'scanNet')
-mmseqs_path = os.path.join(datasets_scanNet_path, 'mmseqs')
-mafft_path = os.path.join(datasets_scanNet_path, 'mafft')
-ASA_path = os.path.join(datasets_scanNet_path, 'ASA')
-ImerFiles_path = os.path.join(datasets_scanNet_path, 'ImerFiles')
 PSSM_path = os.path.join(datasets_scanNet_path, 'PSSM')
+#       QUEEN
 QUEEN_path = os.path.join(datasets_scanNet_path, 'QUEEN')
+entry_dicts_path = os.path.join(QUEEN_path, 'entry_dicts')
+
+#       sources
 scanNet_sources_path = os.path.join(datasets_scanNet_path, 'sources')
 cath_path = os.path.join(scanNet_sources_path, 'cath')
 blast_search_path = os.path.join(scanNet_sources_path, 'blast_search')
 pdbs_path = os.path.join(scanNet_sources_path, 'pdbs')
 assemblies_path = os.path.join(scanNet_sources_path, 'assemblies')
-entry_dicts_path = os.path.join(QUEEN_path, 'entry_dicts')
 
 # models
 models_path = os.path.join(current_dir, 'models')
+QUEEN_model_path = os.path.join(models_path, 'QUEEN')
 # results
 results_path = os.path.join(current_dir, 'results')
 
