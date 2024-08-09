ca6f6b0008282c8302be8436067b1ac618cd7fdb
omri yakir <omriyakir@mail.tau.ac.il>
2024-08-09 09:44:46 +0300

    create scalers and create scaled data for training

--- data_preparation/patch_to_score/data_development.py
+++ data_preparation/patch_to_score/data_development.py
@@ -8,9 +8,10 @@ from data_preparation.ScanNet.db_creation_scanNet_utils import save_as_pickle, l
 import data_development_utils as dev_utils
 import numpy as np
 
+
 def create_merged_protein_object_dict():
     merged_dict = {}
-    for i in range(len(dev_utils.indexes)-1):
+    for i in range(len(dev_utils.indexes) - 1):
         d = load_as_pickle(os.path.join(paths.patches_dicts_path, 'proteinObjectsWithEvoluion' + str(i)))
         for key, value in d.items():
             merged_dict[key] = value
@@ -22,15 +23,38 @@ def create_patches():
     PLDDT_THRESHOLD = 50
     dev_utils.create_patches_dict(i, paths.patches_dicts_path, PLDDT_THRESHOLD)
 
-# def get_uniprots_and_proteins_lists()
+
+def get_uniprots_sequences_and_proteins_lists(merged_dict):
+    all_uniprots = []
+    all_sequences = []
+    all_proteins = []
+    for uniprot, protein in merged_dict.items():
+        all_uniprots.append(uniprot)
+        all_sequences.append(protein.residues)
+        all_proteins.append(protein)
+    return all_uniprots, all_sequences, all_proteins
 
 
 if __name__ == "__main__":
     # CREATE PROTEIN OBJECTS, I'M DOING IT IN BATCHES
     # create_patches()
-    merged_dict = create_merged_protein_object_dict()
-    save_as_pickle(merged_dict, os.path.join(paths.patches_dicts_path, 'merged_protein_objects_with_evolution'))
-    # merged_dict = load_as_pickle(os.path.join(paths.patches_dicts_path, 'merged_protein_objects_with_evolution'))
+    # merged_dict = create_merged_protein_object_dict()
+    # save_as_pickle(merged_dict, os.path.join(paths.patches_dicts_path, 'merged_protein_objects_with_evolution'))
+    merged_dict = load_as_pickle(os.path.join(paths.patches_dicts_path, 'merged_protein_objects_with_evolution'))
+    all_uniprots, all_sequences, all_proteins = get_uniprots_sequences_and_proteins_lists(merged_dict)
+    MAX_NUMBER_OF_COMPONENTS = 10
+    dev_utils.fit_protein_data(all_proteins, paths.scalers_path, MAX_NUMBER_OF_COMPONENTS)
+    scaled_sizes, scaled_components_list, encoded_components_list = (
+        dev_utils.transform_protein_data_list(all_proteins,
+                                              os.path.join(paths.scalers_path,'scaler_size.pkl'),
+                                              os.path.join(paths.scalers_path,'scaler_components.pkl'),
+                                              os.path.join(paths.scalers_path,'encoder.pkl'),
+                                              MAX_NUMBER_OF_COMPONENTS))
+
+    save_as_pickle(scaled_sizes, os.path.join(paths.patch_to_score_data_for_training_path, 'scaled_sizes'))
+    save_as_pickle(scaled_components_list, os.path.join(paths.patch_to_score_data_for_training_path, 'scaled_components_list'))
+    save_as_pickle(encoded_components_list, os.path.join(paths.patch_to_score_data_for_training_path, 'encoded_components_list'))
+
 
     # trainingDataDir = os.path.join(path.predictionsToDataSetDir, dirName)
     # gridSearchDir = os.path.join(path.aggregateFunctionMLPDir, 'MLP_MSA_val_AUC_stoppage_' + dirName)
@@ -39,7 +63,6 @@ if __name__ == "__main__":
     # trainingDictsDir = os.path.join(trainingDataDir, 'trainingDicts')
 
     # plotPlddtHistogramForPositivieAndProteome(allPredictions)
-
 
     # patchesList(allPredictions, int(sys.argv[1]), trainingDataDir, plddtThreshold)
 
