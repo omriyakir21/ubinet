18131c17674849314910089ed596a7e0e448150c
omri yakir <omriyakir@mail.tau.ac.il>
2024-08-20 11:34:15 +0300

    adding util helper to prevent duplication

--- results/patch_to_score/patches_significance.py
+++ results/patch_to_score/patches_significance.py
@@ -1,11 +1,15 @@
 import os
 import sys
+
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
 import numpy as np
 import paths
-from data_preparation.ScanNet.db_creation_scanNet_utils import save_as_pickle,load_as_pickle, THREE_LETTERS_TO_SINGLE_AA_DICT, aa_out_of_chain
-from data_preparation.patch_to_score.data_development_utils import Protein, MAX_NUMBER_OF_COMPONENTS, transform_protein_data_list
-from models.patch_to_score.patch_to_score_MLP_utils import k_computation, prediction_function_using_bayes_factor_computation
+from data_preparation.ScanNet.db_creation_scanNet_utils import save_as_pickle, load_as_pickle, \
+    THREE_LETTERS_TO_SINGLE_AA_DICT, aa_out_of_chain
+from data_preparation.patch_to_score.data_development_utils import Protein, MAX_NUMBER_OF_COMPONENTS, \
+    transform_protein_data_list
+from models.patch_to_score.patch_to_score_MLP_utils import k_computation, \
+    prediction_function_using_bayes_factor_computation
 from results.patch_to_score.patch_to_score_result_analysis import get_best_architecture_path
 import tensorflow as tf
 import pandas as pd
@@ -14,6 +18,7 @@ from sklearn.metrics import auc
 from typing import Dict, List
 import pdb
 
+
 def str_patch_from_list_indexes(residues, list_locations):
     myList = [THREE_LETTERS_TO_SINGLE_AA_DICT[residues[index].resname] + str(residues[index].id[1]) for index in
               list_locations]
@@ -21,7 +26,7 @@ def str_patch_from_list_indexes(residues, list_locations):
     return strPatch
 
 
-def find_model_number(uniprot,uniprots_sets):
+def find_model_number(uniprot, uniprots_sets):
     for i in range(5):
         if uniprot in uniprots_sets[i]:
             return i
@@ -127,37 +132,47 @@ def find_model_number(uniprot,uniprots_sets):
 #     sortedCutted.to_csv(os.path.join(modelsDir, type + '.csv'), index=False)
 #     return sortedCutted
 
-def predict_for_protein(uniprot,scaled_size, scaled_components, encoded_components,models):
-    modelIndex = find_model_number(uniprot)
+def predict_for_protein(uniprot, scaled_size, scaled_components, encoded_components, models):
+    uniprots_sets = load_as_pickle(os.path.join(paths.patch_to_score_data_for_training_path, 'uniprots_sets.pkl'))
+    modelIndex = find_model_number(uniprot, uniprots_sets)
     if modelIndex is None:
-        yhat = np.mean([model.predict([scaled_components, scaled_size, encoded_components]) for model in models], axis=0)
+        yhat = np.mean([model.predict([scaled_components, scaled_size, encoded_components]) for model in models],
+                       axis=0)
     else:
         model = models[modelIndex]
         # pdb.set_trace()
         yhat = model.predict([scaled_components, scaled_size, encoded_components])
     return yhat
 
-def get_patch_to_score_prediction_for_uniprots(all_predictions,plddt_threshold):
+
+def get_info_util(all_predictions, plddt_threshold):
     uniprots = all_predictions['dict_resids'].keys()
     proteins = [Protein(uniprot, plddt_threshold, all_predictions) for uniprot in uniprots]
     scaled_sizes, scaled_components_list, encoded_components_list = (
-    transform_protein_data_list(proteins,
-            os.path.join(paths.scalers_path, 'scaler_size.pkl'),
-            os.path.join(paths.scalers_path, 'scaler_components.pkl'),
-            os.path.join(paths.scalers_path, 'encoder.pkl'),
-            MAX_NUMBER_OF_COMPONENTS))
+        transform_protein_data_list(proteins,
+                                    os.path.join(paths.scalers_path, 'scaler_size.pkl'),
+                                    os.path.join(paths.scalers_path, 'scaler_components.pkl'),
+                                    os.path.join(paths.scalers_path, 'encoder.pkl'),
+                                    MAX_NUMBER_OF_COMPONENTS))
     dir_name = os.path.join(paths.with_MSA_patch_to_score_dir, "50_plddt")
-    best_architecture_path = get_best_architecture_path(dir_name)
-    predictions = []
     models = []
     models_dir_path = get_best_architecture_path(dir_name)
     for filename in os.listdir(models_dir_path):
         if filename.endswith('.keras'):
             models.append(tf.keras.models.load_model(os.path.join(models_dir_path, filename)))
+    return uniprots, proteins, scaled_sizes, scaled_components_list, encoded_components_list, models
+
+
+def get_patch_to_score_prediction_for_uniprots(all_predictions, plddt_threshold):
+    uniprots, proteins, scaled_sizes, scaled_components_list, encoded_components_list, models = get_info_util(
+        all_predictions, plddt_threshold)
+    predictions = []
     for i in range(len(proteins)):
-        yhat = predict_for_protein(uniprots[i],scaled_sizes[i,:], scaled_components_list[i,:,:], encoded_components_list[i,:],models) 
+        yhat = predict_for_protein(uniprots[i], scaled_sizes[i, :], scaled_components_list[i, :, :],
+                                   encoded_components_list[i, :], models)
         predictions.append(yhat)
     return predictions
+
 
 def create_training_ub_ratio(all_predictions):
     all_predictions_ubiq = all_predictions['dict_predictions_ubiquitin']
@@ -168,15 +183,19 @@ def create_training_ub_ratio(all_predictions):
     print(f'ub_ratio: {ub_ratio}')
     save_as_pickle(all_ubiq_predictions, os.path.join(paths.patch_to_score_data_for_training_path, 'ub_ratio.pkl'))
 
+
 def get_top_patches_indices(protein):
     components = protein.connectedComponentsTuples
     if len(components) == 0:
         return None
     sorted_components = sorted(components, key=lambda x: x[1], reverse=True)
-    top_component_sets = [component[4] for component in sorted_components[:min(MAX_NUMBER_OF_COMPONENTS, len(components))]]
+    top_component_sets = [component[4] for component in
+                          sorted_components[:min(MAX_NUMBER_OF_COMPONENTS, len(components))]]
     return top_component_sets
 
-def create_str_patches_and_Significance_for_protein(prediction,uniprot,scaled_size, scaled_components, encoded_components,models,ub_Ratio,protein):
+
+def create_str_patches_and_Significance_for_protein(prediction, uniprot, scaled_size, scaled_components,
+                                                    encoded_components, models, ub_Ratio, protein):
     k_prediction = k_computation(prediction, ub_Ratio)
     inferece_prediction = prediction_function_using_bayes_factor_computation(0.05, k_prediction)
     significances = []
@@ -186,22 +205,22 @@ def create_str_patches_and_Significance_for_protein(prediction,uniprot,scaled_si
         str_patches = [None for i in range(10)]
         significances = ['' for i in range(10)]
         return str_patches, significances
-    
-    indices = tf.argmax(encoded_components, axis=-1) 
+
+    indices = tf.argmax(encoded_components, axis=-1)
     indices_subtracted = tf.maximum(indices - 1, 0)
     one_hot_tensor = tf.one_hot(indices_subtracted, depth=encoded_components.shape[-1])
     for i in range(number_of_patches):
-        without_patch = tf.concat([scaled_components[:i],scaled_components[i+1:]], axis=1)
+        without_patch = tf.concat([scaled_components[:i], scaled_components[i + 1:]], axis=1)
         padded_tensor = tf.keras.preprocessing.sequence.pad_sequences(
-        without_patch, padding="post", maxlen=MAX_NUMBER_OF_COMPONENTS, dtype='float32')
-        yhat = predict_for_protein(uniprot,scaled_size, padded_tensor, one_hot_tensor,models)
+            without_patch, padding="post", maxlen=MAX_NUMBER_OF_COMPONENTS, dtype='float32')
+        yhat = predict_for_protein(uniprot, scaled_size, padded_tensor, one_hot_tensor, models)
         k_value = k_computation(yhat, ub_Ratio)
         inference_yhat = prediction_function_using_bayes_factor_computation(0.05, k_value)
         significances.append(inferece_prediction - inference_yhat)
-    
+
     sorted_significance_indices = np.argsort(np.array(significances))
     sorted_significance = [significances[i] for i in sorted_significance_indices]
-    sorted_significance_padded =  sorted_significance + [0] * (MAX_NUMBER_OF_COMPONENTS - len(sorted_significance))
+    sorted_significance_padded = sorted_significance + [0] * (MAX_NUMBER_OF_COMPONENTS - len(sorted_significance))
     top_component_sets = get_top_patches_indices(protein)
     sorted_component_sets = [top_component_sets[i] for i in sorted_significance_indices]
     for i in range(len(sorted_component_sets)):
@@ -209,20 +228,16 @@ def create_str_patches_and_Significance_for_protein(prediction,uniprot,scaled_si
 
     return sorted_significance_padded, str_patches
 
-def create_str_patches_and_Significances(all_predictions,plddt_threshold,ub_ratio):
-    predictions = get_patch_to_score_prediction_for_uniprots(all_predictions,plddt_threshold)
-    uniprots = all_predictions['dict_resids'].keys()
-    proteins = [Protein(uniprot, plddt_threshold, all_predictions) for uniprot in uniprots]
-    scaled_sizes, scaled_components_list, encoded_components_list = (
-    transform_protein_data_list(proteins,
-            os.path.join(paths.scalers_path, 'scaler_size.pkl'),
-            os.path.join(paths.scalers_path, 'scaler_components.pkl'),
-            os.path.join(paths.scalers_path, 'encoder.pkl'),
-            MAX_NUMBER_OF_COMPONENTS))
-    
+
+def create_str_patches_and_Significances(all_predictions, plddt_threshold, ub_ratio):
+    predictions = get_patch_to_score_prediction_for_uniprots(all_predictions, plddt_threshold)
+    uniprots, proteins, scaled_sizes, scaled_components_list, encoded_components_list, models = get_info_util(
+        all_predictions, plddt_threshold)
     for i in range(len(proteins)):
-        create_str_patches_and_Significance_for_protein(predictions[i],uniprots[i],scaled_sizes[i,:], 
-        scaled_components_list[i,:,:], encoded_components_list[i,:],models,ub_ratio,proteins[i])
+        create_str_patches_and_Significance_for_protein(predictions[i], uniprots[i], scaled_sizes[i, :],
+                                                        scaled_components_list[i, :, :], encoded_components_list[i, :],
+                                                        models, ub_ratio, proteins[i])
+
 
 def create_str_patches_and_Significance10(significance, sorted_locations_cutted, protein):
     structure = protein.getStructure()
@@ -237,6 +252,7 @@ def create_str_patches_and_Significance10(significance, sorted_locations_cutted,
         strPatches[i] = strPatch
         significance10[i] = significance[i][1]
     return strPatches, significance10
+
 
 def filter_dicts_by_keys(data: Dict[str, Dict[str, any]], keys: List[str]) -> Dict[str, Dict[str, any]]:
     """
@@ -255,29 +271,29 @@ def filter_dicts_by_keys(data: Dict[str, Dict[str, any]], keys: List[str]) -> Di
     }
 
 
+# def AF2_work():
+#     allPredictions = utils.loadPickle(os.path.join(path.ScanNetPredictionsPath, 'all_predictions_0304_MSA_True.pkl'))
+#     selected_keys = utils.loadPickle(os.path.join(path.AF2_multimerDir, 'selected_keys_' + str(200) + '.pkl'))
+#     predictions = np.array([get_patch_to_score_prediction_for_uniprots(uniprot) for uniprot in selected_keys])
+#     dict_sources = allPredictions['dict_sources']
+#     labels = np.array([0 if dict_sources[key] in utils.NegativeSources else 1 for key in selected_keys])
+#     ubinetPredictionsDict = {'predictions': predictions, 'labels': labels}
+#     utils.saveAsPickle(ubinetPredictionsDict, os.path.join(path.AF2_multimerDir, 'ubinetLabelsPredictions200'))
+#     precision, recall, thresholds = utils.precision_recall_curve(labels, predictions)
+#     sorted_indices = np.argsort(recall)
+#     sorted_precision = precision[sorted_indices]
+#     sorted_recall = recall[sorted_indices]
+#     aucScore = auc(sorted_recall, sorted_precision)
+#     plt.figure(figsize=(8, 6))
+#     plt.plot(recall, precision, label=f'Precision Recall curve (auc = {aucScore:.2f})')
+#     plt.xlabel('Recall')
+#     plt.ylabel('Precision')
+#     plt.title('AF2 iptm based predictor')
+#     plt.legend()
+#     plt.grid(True)
+#     plt.savefig(os.path.join(path.AF2_multimerDir, 'ubinetPredictor'))
+#     plt.close()
 
-def AF2_work():
-    allPredictions = utils.loadPickle(os.path.join(path.ScanNetPredictionsPath, 'all_predictions_0304_MSA_True.pkl'))
-    selected_keys = utils.loadPickle(os.path.join(path.AF2_multimerDir, 'selected_keys_' + str(200) + '.pkl'))
-    predictions = np.array([get_patch_to_score_prediction_for_uniprots(uniprot) for uniprot in selected_keys])
-    dict_sources = allPredictions['dict_sources']
-    labels = np.array([0 if dict_sources[key] in utils.NegativeSources else 1 for key in selected_keys])
-    ubinetPredictionsDict = {'predictions': predictions, 'labels': labels}
-    utils.saveAsPickle(ubinetPredictionsDict, os.path.join(path.AF2_multimerDir, 'ubinetLabelsPredictions200'))
-    precision, recall, thresholds = utils.precision_recall_curve(labels, predictions)
-    sorted_indices = np.argsort(recall)
-    sorted_precision = precision[sorted_indices]
-    sorted_recall = recall[sorted_indices]
-    aucScore = auc(sorted_recall, sorted_precision)
-    plt.figure(figsize=(8, 6))
-    plt.plot(recall, precision, label=f'Precision Recall curve (auc = {aucScore:.2f})')
-    plt.xlabel('Recall')
-    plt.ylabel('Precision')
-    plt.title('AF2 iptm based predictor')
-    plt.legend()
-    plt.grid(True)
-    plt.savefig(os.path.join(path.AF2_multimerDir, 'ubinetPredictor'))
-    plt.close()
 
 if __name__ == '__main__':
     # all_predictions = load_as_pickle(os.path.join(paths.ScanNet_results_path, 'all_predictions_0304_MSA_True.pkl'))
@@ -287,4 +303,4 @@ if __name__ == '__main__':
     # all_predictions_sub = filter_dicts_by_keys(all_predictions, uniprots)
     # save_as_pickle(all_predictions_sub, os.path.join(paths.patch_to_score_results_path, 'all_predictions_sub.pkl'))
     all_predictions_sub = load_as_pickle(os.path.join(paths.patch_to_score_results_path, 'all_predictions_sub.pkl'))
-    create_str_patches_and_Significances(all_predictions_sub,50,ub_ratio)
+    create_str_patches_and_Significances(all_predictions_sub, 50, ub_ratio)
