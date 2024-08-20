53ff673904e81e76484dd5c41369bc07d71dd38c
Omri Yakir <omriyakir@c-001.cs.tau.ac.il>
2024-08-20 11:33:18 +0300

    paths updates

--- paths.py
+++ paths.py
@@ -67,7 +67,8 @@ results_path = os.path.join(current_dir, 'results')
 ScanNet_results_path = os.path.join(results_path, 'ScanNet')
 #   patch_to_score
 patch_to_score_results_path = os.path.join(results_path, 'patch_to_score')
-
+#       with_MSA
+with_MSA_patch_to_score_dir = os.path.join(patch_to_score_results_path, 'with_MSA')
 
 # tmp
 tmp_path = os.path.join(current_dir, 'tmp')
