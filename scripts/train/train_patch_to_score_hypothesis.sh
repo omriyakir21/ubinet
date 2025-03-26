022045dc3a82249703057a9516feb9ecddebf4d7
Dori Rimon <doririmon@n-102.cs.tau.ac.il>
2025-03-26 14:58:22 +0200

    fixed bug in hypothesis run script

--- scripts/train/train_patch_to_score_hypothesis.sh
+++ scripts/train/train_patch_to_score_hypothesis.sh
@@ -7,5 +7,5 @@ experiment_amount=$(ls -1 $dir_path/*.json | wc -l)
 echo running all $experiment_amount experiments under hypothesis: $hypothesis
 for filename in $dir_path/*.json; do
     echo running experiment: $filename
-    sbatch scripts/train/submit_job_patch_to_score_experiment $filename
+    sbatch scripts/train/submit_job_patch_to_score_experiment.sh $filename
 done
