ef61a7daf42a694b88bd8c1e266ecc160a59b04e
Dori Rimon <doririmon@n-102.cs.tau.ac.il>
2025-03-31 00:18:33 +0300

    bootstrapped sgd and adamw optimizers

--- models/patch_to_score/bootstrappers/optimizer.py
+++ models/patch_to_score/bootstrappers/optimizer.py
@@ -4,8 +4,17 @@ import tensorflow as tf
 def bootstrap_adam(learning_rate: float) -> tf.keras.optimizers.Optimizer:
     return tf.keras.optimizers.Adam(learning_rate=learning_rate)
 
+def bootstrap_sgd(learning_rate: float) -> tf.keras.optimizers.Optimizer:
+    return tf.keras.optimizers.SGD(learning_rate=learning_rate)
+
+def bootstrap_adamw(learning_rate: float) -> tf.keras.optimizers.Optimizer:
+    return tf.keras.optimizers.AdamW(learning_rate=learning_rate)
+
+
 optimizer_to_bootstrapper = {
-    'adam': bootstrap_adam
+    'sgd': bootstrap_sgd,
+    'adam': bootstrap_adam,
+    'adamw': bootstrap_adamw   
 }
 
 def build_optimizer_from_configuration(name: str, kwargs: dict) -> tf.keras.Model:
