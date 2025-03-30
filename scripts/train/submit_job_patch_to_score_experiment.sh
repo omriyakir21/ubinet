9c83adac457625e480ce9bbb847de679fdb9956a
Dori Rimon <doririmon@n-102.cs.tau.ac.il>
2025-03-30 23:02:12 +0300

    changed to killable queue

--- scripts/train/submit_job_patch_to_score_experiment.sh
+++ scripts/train/submit_job_patch_to_score_experiment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #SBATCH --job-name=train_patch_to_score
-#SBATCH --partition=gpu-h100-killable # (see resources section)
+#SBATCH --partition=killable # (see resources section)
 #SBATCH --output=./slurm_output/train_patch_to_score/%j/logs.out
 #SBATCH --time=1440 # max time (minutes)
 #SBATCH --signal=USR1@120 # how to end job when time’s up
