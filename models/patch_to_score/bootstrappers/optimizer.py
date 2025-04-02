1e8ca2152a27bd4905bc31f2837ce2df17953da2
DoriRimon <doririmon111@gmail.com>
2025-04-02 17:45:07 +0300

    fixed bug in adamw optimizer bootstrap

--- models/patch_to_score/bootstrappers/optimizer.py
+++ models/patch_to_score/bootstrappers/optimizer.py
@@ -8,13 +8,15 @@ def bootstrap_sgd(learning_rate: float) -> tf.keras.optimizers.Optimizer:
     return tf.keras.optimizers.SGD(learning_rate=learning_rate)
 
 def bootstrap_adamw(learning_rate: float) -> tf.keras.optimizers.Optimizer:
-    return tf.keras.optimizers.AdamW(learning_rate=learning_rate)
+    # return tf.keras.optimizers.AdamW(learning_rate=learning_rate)  # only supported on tensorflow=2.16.1
+    return tf.optimizers.Adam(learning_rate=learning_rate, weight_decay=0.004)
+
 
 
 optimizer_to_bootstrapper = {
     'sgd': bootstrap_sgd,
     'adam': bootstrap_adam,
-    'adamw': bootstrap_adamw   
+    'adamw': bootstrap_adamw  # TODO : remove this option, and make it a configuration of adam   
 }
 
 def build_optimizer_from_configuration(name: str, kwargs: dict) -> tf.keras.optimizers.Optimizer:
