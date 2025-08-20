1006babdf3dbacb82853797f52638e21964c2d8a
DoriRimon <doririmon111@gmail.com>
2025-08-20 13:53:27 +0300

    zeroed on mask after dense as well in MaskedDense layer

--- models/patch_to_score/models/modules/masked_dense.py
+++ models/patch_to_score/models/modules/masked_dense.py
@@ -6,12 +6,14 @@ class MaskedDense(keras.layers.Dense):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.supports_masking = True
+        
+    def _zero_out_masked(self, tensor, mask) -> tf.Tensor:
+        if mask is not None:
+            tensor = tensor * tf.expand_dims(tf.cast(mask, tensor.dtype), -1)
+        return tensor
 
     def call(self, inputs, mask=None):
-        if mask is not None:
-            inputs = inputs * tf.expand_dims(tf.cast(mask, inputs.dtype), -1)
+        inputs = self._zero_out_masked(inputs, mask)
         outputs = super().call(inputs)
+        outputs = self._zero_out_masked(outputs, mask)
         return outputs
-
-    def compute_mask(self, inputs, mask=None):
-        return mask
